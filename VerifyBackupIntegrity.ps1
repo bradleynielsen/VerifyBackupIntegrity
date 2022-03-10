3fd71e786974491d0a49b575a24bb3039b859ab9
bradleynielsen@gmail.com <bradleynielsen@gmail.com>
2022-03-10 13:57:58 -0600

    function works with source array output

--- VerifyBackupIntegrity.ps1
+++ VerifyBackupIntegrity.ps1
@@ -6,19 +6,51 @@ $sourcePath      = "Z:\backup"
 $destinationPath = "D:\backup"
 
 
-#$sourceFileList      = Get-ChildItem -Path $sourcePath -Recurse -Depth 1
+$sourceFileList      = Get-ChildItem -Path $sourcePath -Recurse
 $destinationFileList = Get-ChildItem -Path $destinationPath -Recurse
 
 
 $sourceCsvPath      = "$PSScriptRoot\sourceHash.csv"
 $destinationCsvPath = "$PSScriptRoot\destinationHash.csv"
 
-$sourceResultsArray      = @()
-$destinationResultsArray = @()
+
+
+function getnerate-HashTable {
+
+    Param (
+        [Parameter(Mandatory = $true)] $FileList
+    )
+
+    $ResultsArray = @()
+
+    foreach ($file in $FileList) {
+	    #Getting file hash 
+        $fileHash = Get-FileHash $file.FullName -Algorithm MD5
+        
+        if ($fileHash){
+            #get path without disk letter
+            $pathOnDisk = ($fileHash.Path).substring(2)
+            #push file hash to table
+            $results = [PSCustomObject]@{
+                Hash = $fileHash.Hash
+                Path = $pathOnDisk
+            }
+            $ResultsArray += $results
+        } else {
+        
+        }
+    }
+
+    return $ResultsArray
+}
+
+
+$sourceResultsArray = getnerate-HashTable -FileList $sourceFileList
+#$destinationResultsArray = getnerate-HashTable -FileList $destinationFileList -ResultsArray $destinationResultsArray
 
 
 
-
+<#
 foreach ($file in $destinationFileList) {
 	#Getting file hash Using MD5 for speed
     $fileHash = Get-FileHash $file.FullName -Algorithm MD5
@@ -35,7 +67,6 @@ foreach ($file in $destinationFileList) {
 }
 
 
-# send to CSV file    
-$destinationResultsArray | Export-Csv -Path $destinationCsvPath -NoTypeInformation
+#>
 
-notepad $destinationCsvPath
+$sourceResultsArray | sort Path

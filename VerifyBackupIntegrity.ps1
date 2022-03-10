5b8d093f78dd827331af8a18112d928ef2874285
bradleynielsen@gmail.com <bradleynielsen@gmail.com>
2022-03-10 12:23:36 -0600

    destination hash table working

--- VerifyBackupIntegrity.ps1
+++ VerifyBackupIntegrity.ps1
@@ -1,1 +1,41 @@
-﻿$PSScriptRoot
+﻿
+#init
+
+
+$sourcePath      = "Z:\backup"
+$destinationPath = "D:\backup"
+
+
+#$sourceFileList      = Get-ChildItem -Path $sourcePath -Recurse -Depth 1
+$destinationFileList = Get-ChildItem -Path $destinationPath -Recurse
+
+
+$sourceCsvPath      = "$PSScriptRoot\sourceHash.csv"
+$destinationCsvPath = "$PSScriptRoot\destinationHash.csv"
+
+$sourceResultsArray      = @()
+$destinationResultsArray = @()
+
+
+
+
+foreach ($file in $destinationFileList) {
+	#Getting file hash Using MD5 for speed
+    $fileHash = Get-FileHash $file.FullName -Algorithm MD5
+    
+    if ($fileHash){
+        $results = [PSCustomObject]@{
+            Path = $fileHash.Path
+            Hash = $fileHash.Hash
+        }
+        $destinationResultsArray += $results
+    } else {
+        
+    }
+}
+
+
+# send to CSV file    
+$destinationResultsArray | Export-Csv -Path $destinationCsvPath -NoTypeInformation
+
+notepad $destinationCsvPath
